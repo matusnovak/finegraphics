cdb19b3647cc6bcf686d8305258ae8d539b88e35
Matus Novak <matusnov@gmail.com>
2018-09-06 20:54:15 +0200

    Fixing warnings

--- tests/traits.h
+++ tests/traits.h
@@ -1,12 +1,14 @@
 #include <type_traits>
 
-#define SECTION_TEST_MOVEABLE(T) SECTION("is moveable") { \
+#define SECTION_TEST_MOVEABLE(T) \
+    SECTION("is moveable") { \
         REQUIRE(std::is_move_constructible<T>::value); \
         REQUIRE(std::is_nothrow_move_constructible<T>::value); \
         REQUIRE(std::is_move_assignable<T>::value); \
     }
 
-#define SECTION_TEST_NOT_COPYABLE(T) SECTION("is NOT copyable") { \
+#define SECTION_TEST_NOT_COPYABLE(T) \
+    SECTION("is NOT copyable") { \
         REQUIRE(std::is_copy_constructible<T>::value == false); \
         REQUIRE(std::is_nothrow_copy_constructible<T>::value == false); \
         REQUIRE(std::is_copy_assignable<T>::value == false); \
